6c8cfe9ed9964e0b08b85a04094e6b93a6a6697a
Ryan Schesnuk <ryanschesnuk@gmail.com>
2017-06-07 16:40:51 -0700

    Add jQuery animations

---
+++ js/animate.js
@@ -0,0 +1,29 @@
+const $portfolioButton = $("#portfolio-button");
+const $root = $("html, body");
+
+$portfolioButton.click(function(event) {
+  event.preventDefault();
+
+  let target = this.hash;
+	let $target = $(target);
+
+	    $root.stop().animate({
+	        'scrollTop': $target.offset().top
+	    }, 700, 'swing');
+});
+
+const $skills = $(".skills-list li");
+
+if ($( window ).width() >= 869) {
+  $skills.hide();
+
+  let i = 0;
+  let intervalSkills = setInterval(function() {
+    if (i < $skills.length) {
+        $skills.eq(i).show("slide", {direction: "right"}, 300);
+        i++;
+    } else {
+      clearInterval(intevalSkills)
+    }
+  }, 100);
+}
